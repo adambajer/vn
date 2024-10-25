5abd29255abf04a4bfc85be79bd6db40d8ad315e
adambajer <adam.bajer@gmail.com>
2024-10-25 22:49:08 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -114,27 +114,13 @@ async function loadSingleNotebookByToken(token) {
         activeNotebookId = notebookId; // Set the active notebook ID globally
         loadNotes(notebookId, 'tokenLoad'); // Pass the source to loadNotes
         updateHeaderWithNotebookInfo(token); // Update the header
-                generateQRCode(token); // Generate QR code for sharing
-
+                
     } else {
         console.error("Invalid notebookToken. No notebook found.");
     }
 }
 
-function generateQRCode(token) {
-    const qrCodeContainer = document.getElementById('qrCodeContainer');
-    if (!qrCodeContainer) {
-        console.error('QR Code container element not found.');
-        return;
-    }
-    const qrCodeUrl = `https://adambajer.github.io/vn/?notebookToken=${token}`;
-    qrCodeContainer.innerHTML = ''; // Clear previous QR code if any
-    new QRCode(qrCodeContainer, {
-        text: qrCodeUrl,
-        width: 128,
-        height: 128
-    });
-}
+ 
 
 async function getNotebookIdByToken(token) {
     const notebooksRef = firebase.database().ref('notebooks');
@@ -156,10 +142,23 @@ async function getNotebookIdByToken(token) {
 function updateHeaderWithNotebookInfo(token) {
     const headerElement = document.getElementById('header'); // Assuming you have a header element with this ID
     if (token) {
-        headerElement.innerHTML = `Notebook token <br> ${token}`;
+        const qrCodeHtml = generateQRCodeHtml(token); // Generate QR code HTML
+        headerElement.innerHTML = `Notebook token <br> ${token} <br> ${qrCodeHtml}`;
     } else {
         headerElement.innerHTML = 'Notebook token <br> not found.';
     }
+}
+
+function generateQRCodeHtml(token) {
+    const qrCodeUrl = `https://adambajer.github.io/vn/?notebookToken=${token}`;
+    const qrCodeContainer = document.createElement('div');
+    qrCodeContainer.id = 'qrCodeContainer';
+    new QRCode(qrCodeContainer, {
+        text: qrCodeUrl,
+        width: 128,
+        height: 128
+    });
+    return qrCodeContainer.outerHTML;
 }
 
 
